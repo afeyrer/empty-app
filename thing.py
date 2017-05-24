0918ada62fefbbeb8973f06d9c6bd0ac24d958c1
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-05-24 14:00:03 -0400

    Updated from Brython Server: 5/24/2017 2:00:02 PM

--- thing.py
+++ thing.py
@@ -3,9 +3,12 @@ from random import randint
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
+dir=7
+dirs=1
+dirss=1
 blue=Color(0x87cefa, 1)
 purple=Color(0x7b68ee, 1)
+pink=Color(0xff69b4, 1)
 line=LineStyle(0,blue)
 black = Color(0, 1)
 bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, line, black)
@@ -14,46 +17,33 @@ pi=3.14159
 go=True
 lineasset=RectangleAsset(1,100,line,blue)
 circle=[Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)),Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100)), Sprite(lineasset, (400,100))]
-circle[1].rotation=pi/8
-circle[2].rotation=pi/4
-circle[3].rotation=pi*3/8
-circle[4].rotation=pi/2
-circle[5].rotation=pi*5/8
-circle[6].rotation=pi*3/4
-circle[7].rotation=pi*7/8
-circle[8].rotation=pi
-circle[9].rotation=pi*9/8
-circle[10].rotation=pi*5/4
-circle[11].rotation=pi*11/8
-circle[12].rotation=pi*3/2
-circle[13].rotation=pi*13/8
-circle[14].rotation=pi*7/4
-circle[15].rotation=pi*15/8
+for x in range(16):
+    circle[x].rotation=pi*x/8
 
 lineasset=RectangleAsset(1,50,line,purple)
 circles=[Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)),Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100)), Sprite(lineasset, (200,100))]
-circles[1].rotation=pi/8
-circles[2].rotation=pi/4
-circles[3].rotation=pi*3/8
-circles[4].rotation=pi/2
-circles[5].rotation=pi*5/8
-circles[6].rotation=pi*3/4
-circles[7].rotation=pi*7/8
-circles[8].rotation=pi
-circles[9].rotation=pi*9/8
-circles[10].rotation=pi*5/4
-circles[11].rotation=pi*11/8
-circles[12].rotation=pi*3/2
-circles[13].rotation=pi*13/8
-circles[14].rotation=pi*7/4
-circles[15].rotation=pi*15/8
+for x in range(16):
+    circles[x].rotation=pi*x/8
+
+lineasset=RectangleAsset(1,30,line,pink)
+circless=[Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)),Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200,500)), Sprite(lineasset, (200, 500)), Sprite(lineasset, (200,500))]
+for x in range(16):
+    circless[x].rotation=pi*x/8
+
+
 def step():
     for x in circle:
         x.rotation+=pi/64
-        x.y+=1
+        x.y+=dir
+        """if x.y+100>=SCREEN_HEIGHT or x.y-100<0:
+            dir=(0-dir)"""
     for x in circles:
         x.rotation+=pi/64
-        x.x+=1
+        x.x+=dirs
+    for x in circless:
+        x.rotation+=pi/64
+        x.x+=dirss
+        x.y-=dirss
 
         
 myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
